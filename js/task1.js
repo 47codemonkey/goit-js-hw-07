b4d33660652d0d4f3f4c76c8df62c585da70eaae
Oleksii <aaalice47@gmail.com>
2019-10-06 19:21:47 +0300

    first commit

---
+++ js/task1.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const categories = document.querySelectorAll("li.item");
+
+console.log(`В списке ${categories.length} категории.`);
+
+for (let i = 0; i < categories.length; i++) {
+    console.log(`Категория: ${categories[i].firstElementChild.innerText}`);
+    console.log(
+        `Количество элементов: ${categories[i].lastElementChild.childElementCount}`
+    );
+}
---
+++ js/task5.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+const checkOutput = e => {
+    if (e.target.value === "") {
+        output.textContent = "незнакомец";
+    } else output.textContent = e.target.value;
+};
+
+input.addEventListener("input", checkOutput);
