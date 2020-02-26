bf30736686f7045a1e72ba3b5e5203b2997c8b83
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-02-26 11:43:01 +0200

    some changes

--- js/task1.js
+++ js/task1.js
@@ -1,12 +1,20 @@
 "use strict";
 
-const categories = document.querySelectorAll("li.item");
+// const categories = document.querySelectorAll("li.item");
 
-console.log(`В списке ${categories.length} категории.`);
+// console.log(`В списке ${categories.length} категории.`);
 
-for (let i = 0; i < categories.length; i++) {
-    console.log(`Категория: ${categories[i].firstElementChild.innerText}`);
-    console.log(
-        `Количество элементов: ${categories[i].lastElementChild.childElementCount}`
-    );
-}
+// for (let i = 0; i < categories.length; i++) {
+//     console.log(`Категория: ${categories[i].firstElementChild.innerText}`);
+//     console.log(
+//         `Количество элементов: ${categories[i].lastElementChild.childElementCount}`
+//     );
+// }
+
+
+
+const categories = document.querySelector('ul#categories')
+console.log(categories.children.length)
+const lists = document.querySelectorAll('li.item')
+lists.forEach(list => console.log(list.firstElementChild.innerText))
+lists.forEach(list => console.log(list.lastElementChild.children.length))
