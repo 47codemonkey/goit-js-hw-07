faedeeebd85c780cc431fc88346effd1fc3453f2
Oleksii Moroz <48028742+aaalice47@users.noreply.github.com>
2020-05-27 10:40:52 +0300

    update

--- js/task1.js
+++ js/task1.js
@@ -6,3 +6,12 @@ console.log(categories.children.length)
 const lists = document.querySelectorAll('li.item')
 lists.forEach(list => console.log(list.firstElementChild.innerText))
 lists.forEach(list => console.log(list.lastElementChild.children.length))
+
+// const categories = document.querySelectorAll('.item')
+
+// categories.forEach(category => {
+//     const cat = category.firstElementChild.textContent
+//     const LM = category.lastElementChild.children.length
+    
+//     console.log(`${cat} and ${LM}`)
+// })
