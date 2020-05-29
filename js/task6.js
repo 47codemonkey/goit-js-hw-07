a7dd80cdc6c6ecbd119cd9af765d5d6a32f66150
Oleksii Moroz <48028742+aaalice47@users.noreply.github.com>
2020-05-29 16:51:04 +0300

    adding better variants to solve the task

--- js/task6.js
+++ js/task6.js
@@ -13,3 +13,54 @@ function val() {
 }
 
 validationInput.addEventListener("blur", val);
+
+
+
+// const input = document.getElementById('validation-input')
+
+// input.addEventListener('blur', fn)
+
+
+// function fn(e) {
+
+//     const six = e.currentTarget.dataset.length
+//     const curVal = e.currentTarget.value.length
+
+//     if(curVal < six || curVal > six) {
+//         input.style.borderColor = 'red'
+//     } else {
+//         input.style.borderColor = 'green'
+
+// }
+
+// }
+
+// function fn(e) {
+
+//     const six = e.currentTarget.dataset.length
+//     const curVal = e.currentTarget.value.length
+
+//     if(curVal < six || curVal > six) {
+
+//         input.classList.add('invalid')
+//     } else {
+//         input.classList.add('valid')
+//         input.classList.remove('invalid')
+
+// }
+
+// }
+
+
+// function fn(e) {
+
+//     const six = Number(e.currentTarget.dataset.length)
+//     const curVal = e.currentTarget.value.length
+
+//     if (curVal === six) {
+//         input.classList.add('valid')
+//     } else {
+//         input.classList.add('invalid')
+//         input.classList.remove('valid')
+//     }
+// }
